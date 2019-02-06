fad8fa977bfb6cce253383c2513c1c1f8ac09956
Shahad Bakoos <shahadbakoos@gmail.com>
2019-02-06 15:13:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,34 +3,20 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 }
-
-
-  BoardMember.prototype.veto = function(){
-    return 'No, I must disagree'
-  }
-  BoardMember.prototype.approve = function(){
-    return 'You can do that!'
-  }
-  BoardMember.prototype.doCharity = function(){
-    return "I like to help people."
-  }
-
-  BoardMember.prototype.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-  }
-
-  BoardMember.prototype.sayHi = function(){
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+ BoardMember.prototype.veto = function(){
+  return 'No, I must disagree'
 }
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+ BoardMember.prototype.approve = function(){
+  return 'You can do that!'
+}
+ BoardMember.prototype.doCharity = function(){
+  return "I like to help people."
+}
+
+ BoardMember.prototype.releasePressStatement = function(){
+  return "You will see great things from Scuber."
+}
+
+ BoardMember.prototype.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
